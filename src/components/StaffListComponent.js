b6d31ff91b3e4910f6d06eebb27bf0310d62a94a
SongLong <songlong2205@gmail.com>
2022-02-12 20:35:36 +0700

    add component

---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class StaffList extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+        );
+
+    }
+}
+
+export default StaffList;
