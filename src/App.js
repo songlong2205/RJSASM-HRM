77cf75b7a0193faeeb687ec09c3279ca31bcd806
SongLong <songlong2205@gmail.com>
2022-02-14 17:41:26 +0700

    commit3

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import StaffList from './components/StaffListComponent';
 import './App.css';
-// import { STAFFS } from '.shared/staffs';
+import { STAFFS } from './shared/staffs';
 // import { Component } from 'react/cjs/react.development';
 
 class App extends Component {
 
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+     super(props);
 
-  //   this.state = {
-  //     staffs: STAFFS
-  //   };
-  // }
+     this.state = {
+       staffs: STAFFS
+     };
+   }
 
   render() {
     return (
@@ -24,7 +24,7 @@ class App extends Component {
             <NavbarBrand href='/'>Ứng dụng quản lý nhân sự v1.0 </NavbarBrand>
           </div>
         </Navbar>
-        <StaffList />
+        <StaffList staffs={this.state.staffs} />
       </div>
     );
   }
