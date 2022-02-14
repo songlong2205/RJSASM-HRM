d3f4020c3c22697ff54b765c82863e8182f0783f
SongLong <songlong2205@gmail.com>
2022-02-14 19:06:42 +0700

    commit4

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class StaffList extends Component {
 
@@ -7,7 +7,36 @@ class StaffList extends Component {
         super(props);
 
         this.state = {
+            selectedStaff: null
+        }
+    }
 
+    onStaffSelect(staff) {
+        this.setState({selectedStaff: staff});
+    }
+
+    renderStaff(staff) {
+        if (staff != null) {
+            return(
+                <div className="col-lg-4 col-md-6 col-sm-12">
+                <Card>
+                    {/* <CardImg width="50%" src={staff.image} alt={staff.name} /> */}
+                    
+                        <p>Họ và tên :{staff.name}</p>
+                        <p>Phòng ban :{staff.name}</p>
+                        <p>Chức danh :{staff.name}</p>
+                        {/* <p>{staff.annualLeave}</p> */}
+                        {/* <CardTitle>{staff.name}</CardTitle>
+                        <CardText>{staff.annualLeave}</CardText> */}
+                    
+                </Card>
+                </div>
+            );
+        }
+        else {
+            return(
+                <div></div>
+            );
         }
     }
 
@@ -16,16 +45,14 @@ class StaffList extends Component {
         const list = this.props.staffs.map((staff) => {
             return (
                 <div key={staff.id} className="col-lg-4 col-md-6 col-sm-12">
-                    <p style={{border : '1px solid grey'}}>{staff.name}</p>
-                    {/* <Media tag="li">
-                        <Media left middle>
-                            <Media object src={staff.name} alt={staff.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                                <Media heading>{staff.name}</Media>
-                                <p>{staff.overTime}</p>
-                        </Media>
-                    </Media> */}
+                    {/* <p onClick={() => this.onStaffSelect(staff)} style={{border : '1px solid grey'}}>{staff.name}</p> */}
+                    <Card onClick={() => this.onStaffSelect(staff)}>
+                    <p>{staff.name}</p>
+                        {/* <CardImg src={staff.image} alt={staff.name} /> */}
+                        {/* <CardImgOverlay>
+                                <CardTitle>{staff.name}</CardTitle>
+                        </CardImgOverlay> */}
+                    </Card>
                 </div>
             );
         });
@@ -33,9 +60,10 @@ class StaffList extends Component {
         return (
             <div className="container-fluid">
                 <div className="row mt-1">
-                    <Media>
                         {list}
-                    </Media>
+                </div>
+                <div className="row">
+                    {this.renderStaff(this.state.selectedStaff)}
                 </div>
                 <p>Bấm vào tên nhân viên để xem thông tin</p>
             </div>
