7de1f9fc549f63726f308acf56c8b6f217a78c07
SongLong <songlong2205@gmail.com>
2022-02-14 16:55:36 +0700

    commit2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import StaffList from './components/StaffListComponent';
 import './App.css';
+// import { STAFFS } from '.shared/staffs';
+// import { Component } from 'react/cjs/react.development';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href='/'>Ứng dụng quản lý nhân sự v1.0 </NavbarBrand>
-        </div>
-      </Navbar>    
-    </div>
-  );
+class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     staffs: STAFFS
+  //   };
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className='container-fluid'>
+            <NavbarBrand href='/'>Ứng dụng quản lý nhân sự v1.0 </NavbarBrand>
+          </div>
+        </Navbar>
+        <StaffList />
+      </div>
+    );
+  }
 }
 
 export default App;
