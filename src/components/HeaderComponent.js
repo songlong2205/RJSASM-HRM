f244aa0690becc275edcaee8d4639691f9e66766
SongLong <songlong2205@gmail.com>
2022-03-02 13:02:39 +0700

    router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
     render() {
         return (
             <>
-                <Navbar dark>
-                    <div className='row'>
-                        <NavbarBrand href='/'>Ứng dụng quản lý nhân sự v1.0 </NavbarBrand>
+                <Navbar dark expand="md">
+                    <div className='container'>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href='/'>
+                            <img src="assets/images/alberto.png" height="30" width="41"
+                                alt="Logo" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-Link" to="/stafflist">
+                                        <span className="fa fa-users"></span>Nhân Viên
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-Link" to="/home">
+                                        <span className="fa fa-address-card-o"></span>Phòng ban
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-Link" to="/aboutus">
+                                        <span className="fa fa-money"></span>Bảng lương
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
             </>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import StaffList from './StaffListComponent';
 import StaffInfo from './StaffInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import '../App.css';
 import { STAFFS } from '../shared/staffs';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -13,7 +15,7 @@ class Main extends Component {
 
         this.state = {
             staffs: STAFFS,
-            selectedStaff: null
+            // selectedStaff: null
         };
     }
 
@@ -22,13 +24,24 @@ class Main extends Component {
     }
 
     render() {
+
+        const HomePage = () => {
+            return(
+                <Home />
+            );
+        }
         return (
-            <div className="App">
+            <div>
                 <Header />
-                <StaffList staffs={this.state.staffs}
-                    onClick={(staffId) => this.onStaffSelect(staffId)} />
-                <StaffInfo
-                    staff={this.state.staffs.filter((staff) => staff.id === this.state.selectedStaff)[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/stafflist" component={() => <StaffList staffs={this.state.staffs} />} /> 
+                    <Redirect to="/home" />
+                </Switch>
+                {/* <StaffList staffs={this.state.staffs}
+                    onClick={(staffId) => this.onStaffSelect(staffId)} /> */}
+                {/* <StaffInfo
+                    staff={this.state.staffs.filter((staff) => staff.id === this.state.selectedStaff)[0]} /> */}
                 <Footer />
             </div>
         );
